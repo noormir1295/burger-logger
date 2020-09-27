1479919846a3ec29a0b7088841c0418cfcd73203
Noor <noormir1295@gmail.com>
2020-09-27 17:31:18 -0400

    adding mysql connection for orm

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+// Set up MySQL connection.
+const mysql= require("mysql");
+
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "burger_db",
+});
+
+//Make connection 
+
+connection.connect(function(err) {
+if(err){
+    console.error("error connecting: " + err.stack);
+    return;
+}
+ console.log("connected as id " + connection.threadId);
+});
+
+//Exporting our connection for orm use
+
+module.exports=connection;
